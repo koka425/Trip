d014ac0475d31fdebebadc16039d39890e089384
Javier <javogx@hotmail.com>
2024-09-22 22:58:05 -0600

    Second commit

--- contact_us.js
+++ contact_us.js
@@ -8,7 +8,15 @@ loginForm.addEventListener("submit", (e) => {
     let dd = document.getElementById('email').value;
     console.log(dd);
     let ee = document.getElementById('mess').value;
-    console.log(ee);
-    document.getElementById('su').innerText = 'CD'
+    if(document.getElementById('mess').value.trim() === '' || document.getElementById('email').value.trim() === '' || document.getElementById('name').value.trim() === ''){
+      document.getElementById('su').innerText = 'Fill all the fields'
+      setTimeout(()=>{
+        document.getElementById('su').innerText = 'Submit'
+      }, 2000)
+    }else{
+      console.log(ee);
+      document.getElementById('su').innerText = 'SUBMITTED'
+    }
+    
     // handle submit
   });
